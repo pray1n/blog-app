d146d91adb9715c955ddccb4edcb5cb429e075a8
pray1n <89249367+pray1n@users.noreply.github.com>
2022-08-13 11:04:54 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
 
 function App() {
+  var contentful = require('contentful');
+  var client = contentful.createClient({
+    space: 'a5pwwbsii6k0',
+    accessToken: 'r8cRv4S5kkPOszbqR32nOugt9yZF-yt007mJM3Mmq0s',
+  });
+  client.getEntry('1vRgHx5IZXSR8ayaeytQnz').then(function (entry) {
+    // logs the entry metadata
+    console.log(entry.sys);
+  
+    // logs the field with ID title
+    console.log(entry.fields.productName);
+  });
   return (
     <p>this is a test</p>
   );
