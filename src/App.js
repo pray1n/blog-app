83184fbd5dac0b6bc0c3c91a9d19b11c167d9396
Ling <abbylinglin@gmail.com>
2022-08-28 00:12:40 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import './App.css';
+import './App.css'
 import GetPost from './components/GetPost'
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header'
+import Footer from './components/Footer'
 import Sidepanel from './components/Sidepanel'
 
-
 function App() {
-  
-  // var contentful = require('contentful');
-  // var client = contentful.createClient({
-  //   space: 'a5pwwbsii6k0',
-  //   accessToken: 'r8cRv4S5kkPOszbqR32nOugt9yZF-yt007mJM3Mmq0s',
-  // });
-  // client.getEntry('1vRgHx5IZXSR8ayaeytQnz').then(function (entry) {
-  //   // logs the entry metadata
-  //   console.log(entry.sys);
-  
-  //   // logs the field with ID title
-  //  console.log(entry.sys.contentType.sys.id)
-  // });
-  // const response = await client.getEntries({ content_type: 'blogPost'})
-  return (
-    <div>
-    <Header />
-    
-    <hr/>
-    <div className="main">
-    <GetPost />
-    </div>
-    <hr/>
-    <Sidepanel />
-    <Footer />
-    </div>
-  );
+    // var contentful = require('contentful');
+    // var client = contentful.createClient({
+    //   space: 'a5pwwbsii6k0',
+    //   accessToken: 'r8cRv4S5kkPOszbqR32nOugt9yZF-yt007mJM3Mmq0s',
+    // });
+    // client.getEntry('1vRgHx5IZXSR8ayaeytQnz').then(function (entry) {
+    //   // logs the entry metadata
+    //   console.log(entry.sys);
+
+    //   // logs the field with ID title
+    //  console.log(entry.sys.contentType.sys.id)
+    // });
+    // const response = await client.getEntries({ content_type: 'blogPost'})
+    return (
+        <div>
+            <Header />
+
+            <div className="main">
+                <GetPost />
+            </div>
+            <hr />
+            <Sidepanel />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
-
+export default App
