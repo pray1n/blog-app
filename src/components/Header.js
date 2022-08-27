5783c7257bc9256baebcdfedb680bb5abd0c9fbf
pray1n <89249367+pray1n@users.noreply.github.com>
2022-08-27 17:55:07 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import CssBaseline from '@mui/material/CssBaseline'
-import useStyles from '@mui/material/useStyles'
-import { Link } from 'react-router-dom' 
+// import React from 'react'
+// import AppBar from '@mui/material/AppBar'
+// import Toolbar from '@mui/material/Toolbar'
+// import CssBaseline from '@mui/material/CssBaseline'
+// // import useStyles from '@mui/material/useStyles'
+// import { Link } from 'react-router-dom' 
 
 // <<<<<<< HEAD
 // function Header (){
@@ -20,35 +20,35 @@ import { Link } from 'react-router-dom'
 //   )
 // =======
 function Header() {
-    const classes = useStyles()
+    // const classes = useStyles()
 
-    return (
-        <AppBar position="static">
-            <CssBaseline />
-            <Toolbar>
-                <div className={classes.logo}>
-                    <img
-                        src="https://img.freepik.com/free-vector/creative-travel-logo-template_23-2148619516.jpg?w=826&t=st=1661601478~exp=1661602078~hmac=c76f0f2fc475a67c4405afe0fe51962a91742f9cf36850d6ed7beb37d04a11f7"
-                        alt="logo"
-                    />
-                </div>
-                <div className={classes.navLinks}>
-                    <Link to="/" className={classes.link}>
-                        Home
-                    </Link>
-                    <Link to="/continents" className={classes.link}>
-                        Continents
-                    </Link>
-                    <Link to="/cities" className={classes.link}>
-                        Cities
-                    </Link>
-                    <Link to="/about" className={classes.link}>
-                        About us
-                    </Link>
-                </div>
-            </Toolbar>
-        </AppBar>
-    )
+    // return 
+    //     <AppBar position="static">
+    //         <CssBaseline />
+    //         <Toolbar>
+    //             <div className={classes.logo}>
+    //                 <img
+    //                     src="https://img.freepik.com/free-vector/creative-travel-logo-template_23-2148619516.jpg?w=826&t=st=1661601478~exp=1661602078~hmac=c76f0f2fc475a67c4405afe0fe51962a91742f9cf36850d6ed7beb37d04a11f7"
+    //                     alt="logo"
+    //                 />
+    //             </div>
+    //             <div className={classes.navLinks}>
+    //                 <Link to="/" className={classes.link}>
+    //                     Home
+    //                 </Link>
+    //                 <Link to="/continents" className={classes.link}>
+    //                     Continents
+    //                 </Link>
+    //                 <Link to="/cities" className={classes.link}>
+    //                     Cities
+    //                 </Link>
+    //                 <Link to="/about" className={classes.link}>
+    //                     About us
+    //                 </Link>
+    //             </div>
+    //         </Toolbar>
+    //     </AppBar>
+    // )
     /* <div className="headercontainer">
             <div className="headerflex">
                 <p>Home</p>
@@ -60,6 +60,7 @@ function Header() {
             </div>
         </div> */
     // >>>>>>> Ling
+//
 }
 
-export default Header
+export default Header;
