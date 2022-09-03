fa9a9ff8ca604d0bfced42d4682147ac7c3af0fb
Ling <abbylinglin@gmail.com>
2022-09-03 11:09:57 +0200

    search bar

--- src/App.js
+++ src/App.js
@@ -4,35 +4,12 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Sidepanel from './components/Sidepanel'
 
-
 function App() {
-  
-  
-  return (
-    
-    <div>
-    <Header />
-    
-    <hr/>
-    <div className="main">
-    <GetPost />
-    </div>
-    <hr/>
-    <Sidepanel />
-    <Footer />
-    </div>
-  );
-}
-
-
-    //   // logs the field with ID title
-    //  console.log(entry.sys.contentType.sys.id)
-    // });
-    // const response = await client.getEntries({ content_type: 'blogPost'})
     return (
         <div>
             <Header />
 
+            <hr />
             <div className="main">
                 <GetPost />
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
@@ -11,6 +10,9 @@ import Container from '@mui/material/Container'
 import Button from '@mui/material/Button'
 import Tooltip from '@mui/material/Tooltip'
 import MenuItem from '@mui/material/MenuItem'
+import { styled, alpha } from '@mui/material/styles'
+import InputBase from '@mui/material/InputBase'
+import SearchIcon from '@mui/icons-material/Search'
 
 const pages = ['Home', 'Continents', 'Cities', 'About Us']
 
@@ -32,6 +34,48 @@ const Header = () => {
     const handleCloseUserMenu = () => {
         setAnchorElUser(null)
     }
+
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.black, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.black, 0.2),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    }))
+
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }))
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: '12ch',
+                '&:focus': {
+                    width: '20ch',
+                },
+            },
+        },
+    }))
 
     return (
         <AppBar sx={{ bgcolor: 'white' }} position="static">
@@ -162,6 +206,15 @@ const Header = () => {
                             open={Boolean(anchorElUser)}
                             onClose={handleCloseUserMenu}></Menu>
                     </Box>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
                 </Toolbar>
             </Container>
         </AppBar>
