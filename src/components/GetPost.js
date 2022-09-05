5b3590ac8fcf12958ae625ac51d82b8f8913ba0e
Tilo Rabs <trabs@web.de>
2022-09-05 19:20:43 +0200

    router

--- src/components/GetPost.js
+++ src/components/GetPost.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect, } from 'react';
 import { client } from '../client';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-const GetPost = ({search,content}) => {
+const GetPost = ({search, content}) => {
     const [PostContent, setPostContent] = useState([]);
-    console.log(content);
+    //console.log(content);
 
     const CleanUpPostContent = (rawData) => {
         const CleanPostContent = rawData.map((post) => {
@@ -24,19 +24,40 @@ const GetPost = ({search,content}) => {
     useEffect(() => {
         const GetPostContent = async () => {
             try {
-                const response = await client.getEntries({ 'metadata.tags.sys.id[in]': 'barcelona',})
-                const responseData = response.items
-                //console.log(response.items)
-                if (responseData) {
-                    CleanUpPostContent(responseData);
+                if(content === 'countries') {
+                    const response = await client.getEntries({ 'metadata.tags.sys.id[in]': 'countries',});
+                    const responseData = response.items;
+                    if (responseData ) {
+                        CleanUpPostContent(responseData);
+                    } else {
+                        setPostContent([]);
+                    }   
+                } else if(content === 'cities') {
+                    const response = await client.getEntries({ 'metadata.tags.sys.id[in]': 'cities',});
+                    const responseData = response.items;
+                    if (responseData ) {
+                        CleanUpPostContent(responseData);
+                    } else {
+                        setPostContent([]);
+                    }   
                 } else {
-                    setPostContent([]);
-                }     
+                    const response1 = await client.getEntries({ 'metadata.tags.sys.id[in]': 'countries',});
+                    const response2 = await client.getEntries({ 'metadata.tags.sys.id[in]': 'cities',});
+                    const responseData1 = response1.items;
+                    const responseData2 = response2.items;
+                    if (responseData1 && responseData2) {
+                        const responseData = [...responseData1,...responseData2];
+                        CleanUpPostContent(responseData);
+                    } else {
+                        setPostContent([]);
+                    }     
+                }
             } catch (error) {
                 console.log(error);
             }
         }
         GetPostContent();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
